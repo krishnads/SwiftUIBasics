08cd2ad5442a9d9fd63b486eab7ce5f3f69e5921
Krishna Datt Shukla <krishna.shukla@closeloop.com>
2019-10-17 11:55:17 +0530

    Hello World example

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Apple on 17/10/19.
+//  Copyright © 2019 Konstant info Solutions Pvt. Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
